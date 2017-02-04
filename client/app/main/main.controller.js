c38f379e7a7121afae5440e424203c9766607ca0
RohitB97 <rohit.bokam97@gmail.com>
2017-02-04 14:24:18 +0530

    major changes

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,34 +4,8 @@
 
   class MainController {
 
-    constructor($http, $scope, socket, Auth, $rootScope, $cookies, $state){
-      $scope.LoggedIn = Auth.isLoggedIn;
-
-      $scope.CurrentUser = Auth.getCurrentUser;
-
-      $scope.goToSettings = function(){
-        $('#viewProfile').modal('hide');
-        setTimeout(function(){$state.go('settings');},1000);
-      };
-
-      //submit receipt
-      
-      $scope.submit = function(){
+    constructor(){
        
-       if(document.getElementById('upload').files.length > 0){
-
-          var id = $cookies.get('userId');
-          var file = document.getElementById('upload').files[0].name;
-
-          $http.put('api/users/accomRequest/'+ id, {name:file}).success(function(response){
-            $('#submit').click();
-            console.log(response);
-          });
-        }    
-      };
-
-     //pop up
-      //setTimeout(function(){$('#popup').modal('show');},2000); 
     }
   }
 
